97253a131f9249270a69de5c2f00197465e79638
TheGeogeo <trolface87@hotmail.fr>
2021-10-10 22:20:45 +0200

    update

---
+++ Clear some trace CW.bat
@@ -0,0 +1,43 @@
+@echo off
+goto check_Permissions
+
+:check_Permissions
+    net session >nul 2>&1
+    if %errorLevel% == 0 (
+        echo Success: Administrative permissions confirmed.
+        goto next
+    ) else (
+        echo Failure: Start it in administrator.
+        goto end
+    )
+
+:next
+reg query "HKEY_LOCAL_MACHINE\Software\WOW6432Node\Blizzard Entertainment"
+if %ERRORLEVEL% EQU 0 reg delete "HKEY_LOCAL_MACHINE\Software\WOW6432Node\Blizzard Entertainment" /f
+reg query "HKEY_CURRENT_USER\Software\Blizzard Entertainment\Battle.net\Identity"
+if %ERRORLEVEL% EQU 0 reg delete "HKEY_CURRENT_USER\Software\Blizzard Entertainment\Battle.net\Identity" /f
+
+if exist %localappdata%\Battle.net\ (
+  RMDIR /S /Q "%localappdata%\Battle.net\"
+)
+if exist %localappdata%\CrashDumps\ (
+  RMDIR /S /Q "%localappdata%\CrashDumps\"
+) 
+if exist %localappdata%\Blizzard Entertainment\ (
+  RMDIR /S /Q "%localappdata%\Blizzard Entertainment\"
+) 
+if exist %appdata%\Battle.net\ (
+  RMDIR /S /Q "%appdata%\Battle.net\" 
+) 
+if exist %programdata%\Battle.net\ (
+  RMDIR /S /Q "%programdata%\Battle.net\" 
+) 
+if exist %programdata%\Blizzard Entertainment\ (
+  RMDIR /S /Q "%programdata%\Blizzard Entertainment\"
+) 
+if exist %UserProfile%\documents\Call Of Duty Black Ops Cold War\ (
+  RMDIR /S /Q "%UserProfile%\documents\Call Of Duty Black Ops Cold War\"
+)
+
+:end
+pause
