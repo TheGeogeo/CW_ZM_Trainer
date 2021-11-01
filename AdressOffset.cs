ff26c8aa22dd88c0bd1b472a885e8d48213e0a77
TheGeogeo <trolface87@hotmail.fr>
2021-11-01 01:18:40 +0100

    up

--- AdressOffset.cs
+++ AdressOffset.cs
@@ -4,11 +4,11 @@ namespace CW_Beach
 {
     internal class AdressOffset
     {
-        //-------------------------------------------------Adress update 1.19.1----------------------------------------------------
+        //-------------------------------------------------Adress update 1.19.2----------------------------------------------------
 
-        public static IntPtr PlayerBase = (IntPtr)0x112F1748; //G_Client
-        public static IntPtr CMDBufferBase = (IntPtr)0xD87C890;
-        public static IntPtr XPScaleBase = (IntPtr)0x11321738;
+        public static IntPtr PlayerBase = (IntPtr)0x1112CD08; //G_Client
+        public static IntPtr CMDBufferBase = (IntPtr)0xD6D18B0;
+        public static IntPtr XPScaleBase = (IntPtr)0x1115CCF8;
 
         //--------------------------------------------------------Offset------------------------------------------------------------
         // If after change adress your game crash it's because offset change probably
